9286222a692971cd5b59ed47835cdab34d28e013
igor <igor@igor.io>
2025-07-31 17:24:14 +0200

    client abstraction

--- server.php
+++ server.php
@@ -4,6 +4,7 @@ namespace ragelord;
 
 use Fiber;
 
+const LISTEN_PORT = 6667;
 const LISTEN_BACKLOG  = 512;
 
 const SELECT_TIMEOUT_SEC  = 10;
@@ -11,74 +12,200 @@ const SELECT_TIMEOUT_USEC = 0;
 
 const CLIENT_READ_SIZE = 1024;
 
-function socket_name($sock) {
+function client_socket_name($sock) {
     socket_getpeername($sock, $addr, $port);
     return "$addr:$port";
 }
 
-$listen_port = 6667;
-$listen_port = 6668; // override to avoid conflict
+enum ClientState {
+    case IDLE;
+    case WAIT_READ;
+    case WAIT_WRITE;
+    case CLOSED;
+    case ERROR;
+}
 
-$server_sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-socket_set_nonblock($server_sock);
-socket_bind($server_sock, '127.0.0.1', $listen_port);
-socket_listen($server_sock, LISTEN_BACKLOG);
+class Client {
+    function __construct(
+        public $name,
+        public $sock,
+        public ClientState $state = CLientState::IDLE,
+        public $readbuf = '',
+        public $writebuf = '',
+        public $fiber = null,
+    ) {}
+
+    function start() {
+        $this->fiber = new Fiber([$this, 'fiber']);
+        $this->fiber->start();
+    }
+
+    function fiber() {
+        echo "{$this->name}: starting fiber\n";
+
+        $msg = $this->read();
+    }
+
+    function read() {
+        echo "read\n";
+        if ($this->state !== ClientState::IDLE) {
+            throw new \RuntimeException(sprintf('invalid state, expected IDLE, got: %s', $this->state));
+        }
+        $this->state = ClientState::WAIT_READ;
+        Fiber::suspend();
+    }
+
+    function write($data) {
+        echo "write\n";
+        if ($this->state !== ClientState::IDLE) {
+            throw new \RuntimeException(sprintf('invalid state, expected IDLE, got: %s', $this->state));
+        }
+        $this->state = ClientState::WAIT_WRITE;
+        $this->writebuf = $data;
+        Fiber::suspend();
+    }
+
+    function serve_read() {
+        echo "serve_read\n";
+        if ($this->state !== ClientState::WAIT_READ) {
+            throw new \RuntimeException(sprintf('invalid state, expected WAIT_READ, got: %s', $this->state));
+        }
+
+        $n = socket_recv($this->sock, $buf, CLIENT_READ_SIZE, MSG_DONTWAIT);
+        if ($n === false) {
+            $this->state !== ClientState::ERROR;
+            throw new \RuntimeException(socket_strerror(socket_last_error()));
+        }
+        if ($n === 0) {
+            $this->close();
+            return;
+        }
+    }
+
+    function serve_write() {
+        echo "serve_write\n";
+        if ($this->state !== ClientState::WAIT_WRITE) {
+            throw new \RuntimeException(sprintf('invalid state, expected WAIT_WRITE, got: %s', $this->state));
+        }
+    }
+
+    function close() {
+        echo "close\n";
+        $this->state = ClientState::CLOSED;
+        socket_write($this->sock, "bye\r\n");
+        socket_close($this->sock);
+    }
+}
+
+function schedule($clients, $read, $write) {
+    printf("schedule: read=%d write=%d\n", count($read), count($write));
+
+    $deleted = [];
+
+    foreach ($read as $sock) {
+        $name = client_socket_name($sock);
+        $client = $clients[$name];
+        if ($client->state === ClientState::WAIT_READ) {
+            $client->serve_read();
+        }
+
+        if ($client->state === ClientState::CLOSED || $client->state === ClientState::ERROR) {
+            $deleted[] = $client;
+        }
+    }
+
+    foreach ($write as $sock) {
+        $name = client_socket_name($sock);
+        $client = $clients[$name];
+        if ($client->state === ClientState::WAIT_WRITE) {
+            $client->serve_write();
+        }
+
+        if ($client->state === ClientState::CLOSED || $client->state === ClientState::ERROR) {
+            $deleted[] = $client;
+        }
+    }
+
+    return $deleted;
+}
+
+function exception_error_handler(
+    int $errno, string $errstr, ?string $errfile = null, ?int $errline  = null
+) {
+    if (!(error_reporting() & $errno)) {
+        // This error code is not included in error_reporting
+        return;
+    }
+    throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+
+set_error_handler(exception_error_handler(...));
+
+$server_sock = null;
+$retries = 0;
+while (true) {
+    try {
+        $server_sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        socket_set_nonblock($server_sock);
+        socket_bind($server_sock, '127.0.0.1', LISTEN_PORT);
+        socket_listen($server_sock, LISTEN_BACKLOG);
+        break;
+    } catch (\ErrorException $e) {
+        if (!str_contains($e->getMessage(), 'Address already in use')) {
+            throw $e;
+        }
+        if ($retries > 60) {
+            echo "retries exceeded\n";
+            throw $e;
+        }
+        if ($retries === 0) {
+            echo "waiting for port to become available...\n";
+        }
+        $retries++;
+        sleep(1);
+    }
+}
+
+echo "ready\n";
 
 $clients = [];
-$fibers = [];
 
 while (true) {
     echo "loop\n";
-    $read = array_values($clients) + [$server_sock];
-    $write = null;
+    $client_socks_read = array_map(
+        fn ($client) => $client->sock,
+        array_filter(array_values($clients), fn ($client) => $client->state === ClientState::WAIT_READ),
+    );
+    $client_socks_write = array_map(
+        fn ($client) => $client->sock,
+        array_filter(array_values($clients), fn ($client) => $client->state === ClientState::WAIT_WRITE),
+    );
+
+    $read = array_merge([$server_sock], $client_socks_read);
+    $write = $client_socks_write;
     $except = null;
+
     $changed = socket_select($read, $write, $except, SELECT_TIMEOUT_SEC, SELECT_TIMEOUT_USEC);
-    var_dump($changed);
     if ($changed === false) {
         throw new \RuntimeException(socket_strerror(socket_last_error()));
     }
-    if ($changed > 0) {
-        foreach ($read as $sock) {
-            if ($sock === $server_sock) {
-                $client_sock = socket_accept($server_sock);
-                socket_set_nonblock($client_sock);
-                $name = socket_name($client_sock);
-                $clients[$name] = $client_sock;
-                $fiber = new Fiber(function ($name) {
-                    echo "$name: starting fiber\n";
 
-                    while (true) {
-                        $msg = Fiber::suspend();
-                        $msg_type = array_shift($msg);
-                        switch ($msg_type) {
-                            case 'read':
-                                [$bytes] = $msg;
-                                echo "$name: bytes received: $bytes\n";
-                            break;
-                            case 'stop':
-                                echo "$name: stop signal received\n";
-                                return;
-                            default:
-                                throw new \InvalidArgumentException("unsupported message type: $msg_type");
-                        }
-                    }
-                });
-                $fiber->start($name);
-                $fibers[$name] = $fiber;
-            } else {
-                $name = socket_name($sock);
-                $buf = null;
-                $n = socket_recv($sock, $buf, CLIENT_READ_SIZE, MSG_DONTWAIT);
-                if ($n === 0) {
-                    // disconnect
-                    socket_close($sock);
-                    $fibers[$name]->resume(['stop']);
-                    unset($fibers[$name]);
-                    unset($clients[$name]);
-                } else {
-                    $fibers[$name]->resume(['read', $buf]);
-                }
-            }
+    if ($changed > 0) {
+        $first = array_key_first($read);
+        if ($first !== null && $read[$first] === $server_sock) {
+            array_shift($read);
+
+            $client_sock = socket_accept($server_sock);
+            socket_set_nonblock($client_sock);
+            $name = client_socket_name($client_sock);
+
+            $clients[$name] = new Client($name, $client_sock);
+            $clients[$name]->start();
+        }
+
+        $deleted = schedule($clients, $read, $write);
+        foreach ($deleted as $client) {
+            unset($clients[$client->name]);
         }
     }
 }
